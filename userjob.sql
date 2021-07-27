762d4112871c2415a54a506aad026e6f6220a901
wushuzhou257 <74419556+wsz263108@users.noreply.github.com>
2021-07-27 11:42:08 +0800

    Add files via upload

---
+++ userjob.sql
@@ -0,0 +1,13 @@
+
+CREATE database sbgl
+
+use sbgl
+
+CREATE TABLE userjob(
+id int(10) PRIMARY KEY auto_increment,
+uname varchar(20) not null,
+money varchar(20) not null,
+pwd VARCHAr(20)
+)
+
+INSERT into userjob VALUES(1,"admit",50,"123456"),(2,"admin",60,"123456"),(3,"123456",70,"123456"),(4,"2234501",80,"123456"),(5,"782269",90,"123456")
